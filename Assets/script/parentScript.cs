1038391c332f6e7970d8481c0fc4dd4c78f5b481
Tavrn <33767561+Tavrn@users.noreply.github.com>
2018-10-31 21:27:54 -0700

    upload

---
+++ Assets/script/parentScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parentScript : MonoBehaviour {
+    public bool Atriggered;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Atriggered = false;
+        }
+	}
+
+
+    public void childTriggered(string name)
+    {
+        if (name == "collider_A" && OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Atriggered = true;
+        } else if (name.Contains("B") && Atriggered && OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("IT WORKED");
+        }
+    }
+}
