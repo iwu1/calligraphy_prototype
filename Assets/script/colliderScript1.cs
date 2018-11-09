1b8830962098331728e2a6986458f7506d51eb9a
iwu1 <iwu@scu.edu>
2018-11-08 18:14:32 -0800

    update script abc creation

---
+++ Assets/script/colliderScript1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colliderScript1 : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter(Collider c) //when you touch it
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            Debug.Log("triggered " + c.name + this.name);
+            parentScript1 pScript1;
+            pScript1 = transform.parent.GetComponent<parentScript1>();
+            Debug.Log("...working" + pScript1);
+            pScript1.childTriggered(gameObject.name);
+        }
+    }
+
+    /*void OnTriggerExit(Collider c ) //when you leave it
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            Debug.Log("exit " + c.name + this.name);
+            parentScript pScript;
+            pScript = transform.parent.GetComponent<parentScript>();
+        }
+    }*/
+}
---
+++ Assets/script/parentScript1.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parentScript1 : MonoBehaviour {
+    public MeshRenderer mRend;//material renderer of the calligrpahy character
+    public Material mat; //material you want to switch to
+
+
+    public bool Atriggered;
+    public bool Btriggered;
+    // bool Btriggered;
+    // Use this for initialization
+    void Start () {
+        mRend = gameObject.GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            //Atriggered = false;
+            /*
+            if (Atriggered && Btriggered)
+            {
+                mRend.material = mat; //change the material to the material you wanted it to change to
+            }
+            else if (Atriggered)
+            {
+                Atriggered = false;
+            }
+            */
+        }
+	}
+
+
+    public void childTriggered(string name)
+    {
+        Debug.Log("trigger on parent");
+        if (name.Contains ("collider_A") )//&& OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Atriggered = true;
+        }
+        if (name.Contains ("collider_B") && Atriggered)
+        {
+            Btriggered = true;
+        }
+        if (name.Contains ("collider_C") && Atriggered && Btriggered)
+        {
+            mRend.material = mat; //change the material to the material you wanted it to change to
+        }
+    }
+
+    /*
+    public void childExit(string name)
+    {
+        if (name == "collider_B")
+        {
+            Btriggered = false;
+        }
+    }
+    */
+}
