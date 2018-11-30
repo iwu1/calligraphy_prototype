d8447754d608276b26dca38209c1a3d4b9687e6b
iwu1 <iwu@scu.edu>
2018-11-29 20:22:56 -0800

    reset buttons

---
+++ Assets/script/numberColorReset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class numberColorReset : MonoBehaviour
+{
+    public MeshRenderer three_a;
+    public MeshRenderer three_b;
+    public MeshRenderer three_c;
+    public Material mat;
+    public void OnTriggerEnter(Collider Col)
+    {
+        if (Col.tag == "Player")
+        {
+            three_a.material = mat;
+            three_a.gameObject.GetComponent<parentScript>().Atriggered= false;
+            three_b.material = mat;
+            three_b.gameObject.GetComponent<parentScript>().Atriggered = false;
+            three_c.material = mat;
+            three_c.gameObject.GetComponent<parentScript>().Atriggered = false;
+
+        }
+    }
+}
---
+++ Assets/script/waterColorReset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterColorReset : MonoBehaviour {
+    public MeshRenderer water_a;
+    public MeshRenderer water_b;
+    public MeshRenderer water_c;
+    public MeshRenderer water_d;
+    public Material mat;
+    public void OnTriggerEnter (Collider Col)
+    {
+        if (Col.tag == "Player")
+        {
+            water_a.material = mat;
+            water_a.gameObject.GetComponent<parentScript1>().Atriggered = false;
+            water_a.gameObject.GetComponent<parentScript1>().Btriggered = false;
+            water_b.material = mat;
+            water_b.gameObject.GetComponent<parentScript1>().Atriggered = false;
+            water_a.gameObject.GetComponent<parentScript1>().Btriggered = false;
+            water_c.material = mat;
+            water_c.gameObject.GetComponent<parentScript>().Atriggered = false;
+            water_d.material = mat;
+            water_d.gameObject.GetComponent<parentScript>().Atriggered = false;
+        }
+    }
+}
